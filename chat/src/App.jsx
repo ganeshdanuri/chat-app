9c84f741d7fcd245e144fe9fb522a71e9bc0fa3f
ganeshdanuri <danuri.ganesh143@gmail.com>
2023-10-20 14:34:46 +0530

    fixed styling

--- chat/src/App.jsx
+++ chat/src/App.jsx
@@ -1,11 +1,13 @@
 import Chat from "./components/Chat";
 import Sidebar from "./components/Sidebar";
 
+import "./App.css";
+
 export const App = () => {
   const userInfo = JSON.parse(localStorage.getItem("userInfo"));
 
   return (
-    <div className="flex flex-row w-full h-screen">
+    <div className="flex flex-row w-full h-screen app-container">
       <Sidebar userInfo={userInfo} />
       <Chat userInfo={userInfo} />
     </div>
